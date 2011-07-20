52f5463d6700aa965aecf229505fd21a0e1994c9
Don Schwarz <don.schwarz@gmail.com>
2011-07-20 16:40:37 -0500

    Various cleanups.

--- main.py
+++ main.py
@@ -3,32 +3,52 @@ try:
 except ImportError:
   import json
 
-import sys
 import urllib2
+import sys
 
 from google.appengine.ext import webapp
-from google.appengine.ext import db
+from google.appengine.api import urlfetch
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+import metadata
+import models
 
-class View(db.Model):
-    id = db.StringProperty()
-    name = db.StringProperty()
-    column_id = db.StringProperty()
+
+# Approximate Chicago area boundary.
+BOUNDS = '41.496235,-87.978516|42.106374,-87.138062'
 
 class QueryPage(webapp.RequestHandler):
     def get(self):
-        lat = float(self.request.get('lat'))
-        lng = float(self.request.get('lng'))
-        range = long(self.request.get('range'))
+        address = self.request.get('address')
+        if address:
+          # Note that in its current form, this application violates
+          # the Google Maps Geocoder ToS.  You must display the
+          # results on a map.  We plan to do this in the future but
+          # are not yet.
+          r = urlfetch.fetch('http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false&bounds=%s' % (
+              urllib2.quote(address), BOUNDS))
+          if r.status_code == 200:
+            j = json.loads(r.content)
+            if j['status'] == "OK":
+              lat = j['results'][0]['geometry']['location']['lat']
+              lng = j['results'][0]['geometry']['location']['lng']
+            else:
+              self.response.out.write('Could not find address %s: %s' % (
+                  address, j['status']))
+              return
+        else:
+          lat = float(self.request.get('lat'))
+          lng = float(self.request.get('lng'))
+        range = long(self.request.get('range'))  # Meters
         self.response.headers['Content-Type'] = 'text/plain'
-        for view in View.all():
+        rpcs = []
+        for view in models.View.all():
             view_id = view.id
             column_id = long(view.column_id)
             data = json.dumps(
                 {
                     "originalViewId": view_id,
-                    "name": "BillsMom",
+                    "name": view.name,
                     "query": {
                         "filterCondition": {
                             "type": "operator",
@@ -70,39 +90,32 @@ class QueryPage(webapp.RequestHandler):
                         }
                     })
             url = "http://data.cityofchicago.org/api/views/INLINE/rows.json?method=index"
-            request = urllib2.Request(url, data, headers={'Content-type:' : 'application/json'})
-            results = json.loads(urllib2.urlopen(request).read())
-            # TODO: Obviously we need much better formatting here.
-            if results['data']:
-              self.response.out.write("*** %s *****\n" % view.name)
-              for d in results['data']:
-                self.response.out.write(' '.join(str(x).encode('utf-8') for x in d))
-                self.response.out.write('\n')
-        
-
-class GeneratePage(webapp.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        for page in xrange(1, 12):
-            f = urllib2.urlopen('http://data.cityofchicago.org/api/views?page=%d&limit=50' % page).read()
-            results = json.loads(f)
-            for r in results:
-                id = r['id']
-
-                f = urllib2.urlopen('http://data.cityofchicago.org/api/views/%s/columns.json' % id).read()
-                for c in json.loads(f):
-                    if c['dataTypeName'] == 'location':
-                        self.response.out.write(
-                            "%s (%s), col name = %s\n" % (r['name'], id, c['name']))
-                        v = View(id = id,
-                                 name = r['name'],
-                                 column_id = str(c['id']))
-                        v.put()
+            rpc = urlfetch.create_rpc()
+            urlfetch.make_fetch_call(rpc, url,
+                                     headers={'Content-type:' : 'application/json'},
+                                     method=urlfetch.POST,
+                                     payload=data)
+            rpcs.append(rpc)
+        for rpc in rpcs:
+            rpc.wait()
+            result = rpc.get_result()
+            if result.status_code == 200:
+              response = json.loads(result.content)
+              # TODO: Obviously we need much better formatting here.
+              if response['data']:
+                self.response.out.write("%s\n" % response['meta']['view']['name'])
+                for d in response['data']:
+                  for i in xrange(len(d)):
+                    col = response['meta']['view']['columns'][i]
+                    if col['dataTypeName'] == 'text':
+                      col_name = col['name']
+                      data = d[i]
+                      self.response.out.write('  %s: %s\n' % (col_name, data))
+                  self.response.out.write('\n')
 
 
 application = webapp.WSGIApplication(
                                      [('/query', QueryPage),
-                                      ('/generate', GeneratePage),
                                       ],
                                      debug=True)
 
